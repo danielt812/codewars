1f457043f4f26fb06a38cc6ba36e22e8ccc586c2
Daniel Tolan <danieltolan@gmail.com>
2023-07-12 12:24:02 -1000

    findAverage.js

--- javascript/evenOrOdd.js
+++ javascript/evenOrOdd.js
@@ -1,24 +1,25 @@
-import { assert } from "chai";
-
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+import { assert } from 'chai';
+
 const evenOrOdd = (num) => {
-  return num % 2 === 0 ? "Even" : "Odd";
+	return num % 2 === 0 ? 'Even' : 'Odd';
 };
 
-describe("evenOrOdd", () => {
-  it("2 is even", () => {
-    assert.strictEqual(evenOrOdd(2), "Even");
-  });
-  it("7 is odd", () => {
-    assert.strictEqual(evenOrOdd(7), "Odd");
-  });
-  it("-42 is even", () => {
-    assert.strictEqual(evenOrOdd(-42), "Even");
-  });
-  it("-7 is odd", () => {
-    assert.strictEqual(evenOrOdd(-7), "Odd");
-  });
-  it("0 is even", () => {
-    assert.strictEqual(evenOrOdd(0), "Even");
-  });
+describe('evenOrOdd Basic tests', () => {
+	it('2 is even', () => {
+		assert.strictEqual(evenOrOdd(2), 'Even');
+	});
+	it('7 is odd', () => {
+		assert.strictEqual(evenOrOdd(7), 'Odd');
+	});
+	it('-42 is even', () => {
+		assert.strictEqual(evenOrOdd(-42), 'Even');
+	});
+	it('-7 is odd', () => {
+		assert.strictEqual(evenOrOdd(-7), 'Odd');
+	});
+	it('0 is even', () => {
+		assert.strictEqual(evenOrOdd(0), 'Even');
+	});
 });
