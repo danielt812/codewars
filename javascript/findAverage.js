fd3fc60ae7afa62d9fc9334ec51185ac681af04c
Daniel Tolan <danieltolan@gmail.com>
2023-07-12 14:33:14 -1000

    findAverage solution

--- javascript/findAverage.js
+++ javascript/findAverage.js
@@ -1,20 +1,20 @@
 // Write a function that accepts two parameters (sum and multiply) and find two numbers [x, y], where x + y = sum and x * y = multiply.
 
-import { assert } from 'chai';
+import { assert } from "chai";
 
 const findAverage = (arr) => arr.reduce((acc, cur) => acc + cur, 0) / arr.length || 0;
 
-describe('findAverage Basic tests', () => {
-	it('Expected value: 1', () => {
-		assert.strictEqual(findAverage([1, 1, 1]), 1);
-	});
-	it('Expected value: 2', () => {
-		assert.strictEqual(findAverage([1, 2, 3]), 2);
-	});
-	it('Expected value: 2', () => {
-		assert.strictEqual(findAverage([1, 2, 3]), 2);
-	});
-  it('Expected value: 0', () => {
-		assert.strictEqual(findAverage([]), 2);
-	});
+describe("findAverage Basic tests", () => {
+  it("Expected value: 1", () => {
+    assert.strictEqual(findAverage([1, 1, 1]), 1);
+  });
+  it("Expected value: 2", () => {
+    assert.strictEqual(findAverage([1, 2, 3]), 2);
+  });
+  it("Expected value: 2", () => {
+    assert.strictEqual(findAverage([1, 2, 3]), 2);
+  });
+  it("Expected value: 0", () => {
+    assert.strictEqual(findAverage([]), 2);
+  });
 });
