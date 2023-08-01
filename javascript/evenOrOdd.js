6668b758244d1bba22b7d1c42b912c3ec78bee85
Daniel Tolan <danieltolan@gmail.com>
2023-07-31 16:33:37 -1000

    findCapitals.js added

--- javascript/evenOrOdd.js
+++ javascript/evenOrOdd.js
@@ -1,25 +1,25 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-import { assert } from 'chai';
+import { assert } from "chai";
 
 const evenOrOdd = (num) => {
-	return num % 2 === 0 ? 'Even' : 'Odd';
+	return num % 2 === 0 ? "Even" : "Odd";
 };
 
-describe('evenOrOdd Basic tests', () => {
-	it('2 is even', () => {
-		assert.strictEqual(evenOrOdd(2), 'Even');
+describe("evenOrOdd Basic tests", () => {
+	it("2 is even", () => {
+		assert.strictEqual(evenOrOdd(2), "Even");
 	});
-	it('7 is odd', () => {
-		assert.strictEqual(evenOrOdd(7), 'Odd');
+	it("7 is odd", () => {
+		assert.strictEqual(evenOrOdd(7), "Odd");
 	});
-	it('-42 is even', () => {
-		assert.strictEqual(evenOrOdd(-42), 'Even');
+	it("-42 is even", () => {
+		assert.strictEqual(evenOrOdd(-42), "Even");
 	});
-	it('-7 is odd', () => {
-		assert.strictEqual(evenOrOdd(-7), 'Odd');
+	it("-7 is odd", () => {
+		assert.strictEqual(evenOrOdd(-7), "Odd");
 	});
-	it('0 is even', () => {
-		assert.strictEqual(evenOrOdd(0), 'Even');
+	it("0 is even", () => {
+		assert.strictEqual(evenOrOdd(0), "Even");
 	});
 });
